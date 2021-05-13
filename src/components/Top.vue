313de29560f8f27dec7c3f0f8dce8d515ed18c57
Ragnarok <juankcontreras00@gmail.com>
2021-05-12 23:58:48 -0500

    First Commit

---
+++ src/components/Top.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <router-link class="nav-link" :to="{name: 'home'}" title="Go Home">
+        <img src="@/assets/mylogo2.png" style="max-height: 50px; max-width: 80px">
+      </router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse"
+            data-target="#navbarNav" aria-controls="navbarNav"
+            aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <router-link class="nav-link" :to="{name: 'about'}">
+            About
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <div class="col" style="text-align: end">
+        <span class="navbar-text">
+          Made by Ragnarok
+        </span>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Top',
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="@/assets/volcano1.jpg" class="d-block w-100">
+          <div class="carousel-caption d-none d-md-block">
+            <h1 style="font-family: 'Noto Sans CJK SC'">Welcome to the volcanoe's world!!!</h1>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="@/assets/volcano2.jpg" class="d-block w-100">
+        </div>
+        <div class="carousel-item">
+          <img src="@/assets/volcano3.jpg" class="d-block w-100">
+        </div>
+        <div class="carousel-item">
+          <img src="@/assets/volcano4.png" class="d-block w-100">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators"
+         role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators"
+         role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
